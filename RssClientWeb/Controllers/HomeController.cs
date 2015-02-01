eff1ef49dfd1fc450b28993b57c3db39cfdb9c70
steelcm <csteel@gmail.com>
2015-02-01 18:17:59 +0000

    Basic feed info mapped to DTO ready for saving to DB

--- RssClientWeb/Controllers/HomeController.cs
+++ RssClientWeb/Controllers/HomeController.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
+using System.ServiceModel.Web;
+using System.ServiceModel.Syndication;
+using RssClientModels.ViewModels;
+using RssClientWeb.Managers;
 
 namespace RssClientWeb.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public ActionResult Index()
+        private readonly IFeedManager _feedManager;
+ 
+        public HomeController(IFeedManager feedManager)
         {
-            return View();
+            this._feedManager = feedManager;
+        }
+
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index(string inputURL)
+        {
+            if (string.IsNullOrEmpty(inputURL))
+            {
+                return null;
+            }
+            var feedDTO = _feedManager.AddFeed(new Uri(inputURL));
+            return View(feedDTO);
         }
     }
 }
